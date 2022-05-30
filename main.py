0401914b67901bc0b4164e1f014a45991710c8c9
Olyno <olyno.dev@gmail.com>
2022-05-30 21:38:05 +0200

    improve code

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def printUsage():
   print("Usage: ./main.py <domain name>")
   sys.exit(0)
 
-def getFoundUrls(domain_name):
+def getFoundUrls(domain):
   file_path = path.relpath("SecLists/Discovery/Web-Content/common.txt")
   urls = []
   with yaspin() as sp:
@@ -20,16 +20,24 @@ def getFoundUrls(domain_name):
     with open(file_path) as f:
       for url_path in f:
         sp.text = "Checking common urls: /{0}".format(url_path.strip())
-        url = "http://{0}/{1}".format(domain_name, url_path)
+        url = "{0}/{1}".format(domain, url_path)
         r = requests.get(url)
         if r.status_code == 200:
           urls.append(url)
     return urls
 
 def getDatas(domain_name):
+  domain = domain_name
+  r = requests.get("https://" + domain)
+  if r.status_code == 200:
+    domain = "https://{0}".format(domain)
+  else:
+    domain = "https://{0}".format(domain)
+
   domain_ip = socket.gethostbyname(domain_name)
   domain_infos = whois.query(domain_name)
-  url_founds = getFoundUrls(domain_name)
+  domain_aditional_infos = getAdditionalWhoisInfo(domain_name)
+  url_founds = getFoundUrls(domain)
   return """
 # Informations about {0}
 
@@ -38,12 +46,14 @@ Registrar: {2}
 Registrant: {3}
 Country: {4}
 Created At: {5}
+Expires At: {6}
+Last Updated At: {7}
 
 Nameservers:
-  * {6}
+  * {8}
 
 URLs found:
-  * {7}
+  * {9}
   """.format(
     domain_name,
     domain_ip,
@@ -51,6 +61,9 @@ URLs found:
     domain_infos.registrant,
     domain_infos.registrant_country,
     domain_infos.creation_date,
+    domain_infos.expiration_date,
+    domain_infos.last_updated,
+    domain_infos.last_updated,
     "\n  * ".join( list(domain_infos.name_servers) ),
     "\n  * ".join(url_founds)
   )
